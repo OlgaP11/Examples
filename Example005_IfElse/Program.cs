c25553517e8a3e1c0a444702800346163052af20
OlgaP11 <ooolchaaa@inbox.ru>
2022-10-12 17:03:16 +0300

    Add Example 005 IfElse

---
+++ Example005_IfElse/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите имя пользователя");
+string userName = Console.ReadLine();
+if (userName.ToLower() == "маша") 
+{
+    Console.WriteLine ("Ура! Это же Маша!");
+} 
+else 
+{
+    Console.Write ("Привет, ");
+    Console.WriteLine (userName);
+}
